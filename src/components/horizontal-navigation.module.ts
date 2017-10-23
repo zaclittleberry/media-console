b57a3d070f2afb0c05a7d83d220c5b0c24d67980
Zac Littleberry <zaclittleberry@gmail.com>
2017-10-23 00:10:19 -0400

    add horizontal navigation component

---
+++ src/components/horizontal-navigation.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
+import { HorizontalNavigationComponent } from 'horizontal-navigation';
+
+@NgModule({
+  declarations: [
+    HorizontalNavigationComponent,
+  ],
+  imports: [
+    IonicModule,
+  ],
+  exports: [
+    HorizontalNavigationComponent,
+  ]
+})
+export class HorizontalNavigationComponentModule {}
---
+++ src/components/horizontal-navigation.ts
@@ -0,0 +1,84 @@
+import { Component, ElementRef, ViewChild, AfterViewChecked } from '@angular/core';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'horizontal-navigation',
+  templateUrl: 'horizontal-navigation.html',
+})
+export class HorizontalNavigationComponent implements AfterViewChecked {
+
+  navItems: any;
+
+  scrollChange: number;
+
+  prevMaxScrollOffset = 0;
+
+  @ViewChild('scrollArea') scrollArea: ElementRef;
+  @ViewChild('scrollLeft') scrollLeft: ElementRef;
+  @ViewChild('scrollRight') scrollRight: ElementRef;
+
+  constructor (
+    private dataService: DataService,
+  ) {
+    this.scrollChange = 20;
+  }
+
+  ngOnInit() {
+    this.receiverInputInputs();
+
+    this.scrollArea.nativeElement.onscroll = (function() {
+      console.log("onscroll");
+      this.checkScroll();
+    }).bind(this);
+  }
+
+  ngOnDestroy() {
+    // remove scroll listener
+  }
+
+  ngAfterViewChecked() {
+    let scrollElement = this.scrollArea.nativeElement;
+    let maxScrollOffset = scrollElement.scrollWidth - scrollElement.clientWidth;
+    if (maxScrollOffset === this.prevMaxScrollOffset) {
+    } else {
+      this.prevMaxScrollOffset = maxScrollOffset;
+      this.checkScroll();
+    }
+  }
+
+  checkScroll() {
+    let scrollElement = this.scrollArea.nativeElement;
+    let scrollOffset = scrollElement.scrollLeft;
+    if (scrollOffset <= 0) {
+      this.scrollLeft.nativeElement.classList.add("disabled");
+    } else {
+      this.scrollLeft.nativeElement.classList.remove("disabled");
+    }
+    let maxScrollOffset = scrollElement.scrollWidth - scrollElement.clientWidth;
+    if (scrollOffset >= maxScrollOffset) {
+      this.scrollRight.nativeElement.classList.add("disabled");
+    } else {
+      this.scrollRight.nativeElement.classList.remove("disabled");
+    }
+  }
+
+  receiverInputInputs() {
+    this.dataService.receiverInputInputs().subscribe(response => {
+      this.navItems = response;
+    });
+  }
+
+  navAction(input) {
+    this.dataService.receiverInputSet(input).subscribe(response => {
+
+    });
+  }
+
+  scrollTowardsEnd() {
+    this.scrollArea.nativeElement.scrollLeft += this.scrollChange;
+  }
+
+  scrollTowardsStart() {
+    this.scrollArea.nativeElement.scrollLeft -= this.scrollChange;
+  }
+}
